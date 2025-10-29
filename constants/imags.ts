b35987d2c2cda24be6ebdda550e652977289f90d
omar <omarqais225@gmail.com>
2025-10-29 14:45:59 +0200

    setup colors and icons for auth screens

---
+++ constants/imags.ts
@@ -0,0 +1,7 @@
+export const emailIcon = require('../assets/icons/email.png');
+export const lockIcon = require('../assets/icons/padlock.png');
+export const userIcon = require('../assets/icons/user.png');
+export const closeEyeIcon = require('../assets/icons/eye.png');
+export const viewEyeIcon = require('../assets/icons/view.png');
+export const starIcon = require('../assets/icons/star.png');
+export const arrowIcon = require('../assets/icons/curved-arrow-with-broken-line.png');
